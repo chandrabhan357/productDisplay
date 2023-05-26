ab3c9c810e1417a2c5461f5fc846b5347b489fb6
chandrabhan357 <chandrabhan1366@hotmail.com>
2023-05-26 17:34:59 +0530

    Added my project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>  
+<SkillAll />
+</template>
+
+<script>
+import SkillAll from './components/SkillAll.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SkillAll
+    
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+body {
+  background-color: #f2f2f2;
+  margin: 0px;
+  font-family: tahoma;
+  color: #282828;
+}
+</style>
---
+++ src/components/SkillAll.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="nav-bar"></div>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <!-- image goes here -->
+      </div>
+      <div class="product-info">
+        <h1>{{ product }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SkillAll",
+  data() {
+    return {
+      product: "Socks",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.nav-bar {
+  background: linear-gradient(-90deg, #84cf6a, #16c0b0);
+  height: 60px;
+  margin-bottom: 25px;
+  -webkit-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  -moz-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.57);
+}
+
+.product-display {
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+}
+
+.product-container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
