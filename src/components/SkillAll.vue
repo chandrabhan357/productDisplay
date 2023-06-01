849b9284926c1359e8746b4303b860781ce43660
chandrabhan357 <chandrabhan1366@hotmail.com>
2023-06-01 19:43:29 +0530

    Added Computed Properties

--- src/components/SkillAll.vue
+++ src/components/SkillAll.vue
@@ -1,60 +1,77 @@
 <template>
   <div class="nav-bar"></div>
+  <div class="cart">Cart({{ cart }})</div>
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
-        <img v-bind:src="image">
+        <img v-bind:src="image" />
       </div>
       <div class="product-info">
-        <h1>{{ product }}</h1>
-        <p v-if="inventory > 10">In Stock</p>
-            <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p>
-            <p v-else>Out of Stock</p>
-            <ul>
-              <li v-for=" (detail, index) in details" :key="index">{{ detail }}</li>
-            </ul>
-            <ul>
-              <li v-for="(size, index) in sizes" :key="index">{{ size }}</li>
-            </ul>
-            <div v-for="variant in variants" :key="variant.id" @mouseover="updateImage(variant.image)">{{ variant.color }}</div>
-            <button class="button" v-on:click="addToCart">Add to Cart</button>
+        <h1>{{ title }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <ul class="list">
+          <li v-for="(detail, ind) in details" :key="ind">{{ detail }}</li>
+        </ul>
+        <div
+          v-for="(variant, index) in variants"
+          :key="variant.id"
+          @click="updateVariant(index)"
+          class="color-circle"
+          :style="{ backgroundColor: variant.color }"
+        ></div>
+        <button
+          class="button"
+          :class="{ disabledButton: !inStock }"
+          :disbled="!inStock"
+          v-on:click="addToCart"
+        >
+          Add to Cart
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Bluesocks from '../assets/socks_blue.jpg'
-import Greensocks from '../assets/socks_green.jpg'
+import Bluesocks from "../assets/socks_blue.jpg";
+import Greensocks from "../assets/socks_green.jpg";
 
 export default {
   name: "SkillAll",
   data() {
     return {
       cart: 0,
+      brand: "Vue Mastery",
       product: "Socks",
-      image: Bluesocks,
-      inventory:100,
-      details: ['50% cotton', '30% wool', '20% polyester'],
+      selectedVariant: 0,
+      details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-              { id: 2234, color: 'green', image: Greensocks },
-              { id: 2235, color: 'blue', image: Bluesocks  },
-            ],
-            sizes: ['S', 'M', 'L', 'XL']
-    }
+        { id: 2234, color: "green", image: Greensocks, quantity: 50 },
+        { id: 2235, color: "blue", image: Bluesocks, quantity: 0 },
+      ],
+    };
   },
-    methods: {
-      addToCart() {
-            this.cart += 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-  
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateVariant(index) {
+      this.selectedVariant = index
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+  },
 };
-
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -68,6 +85,45 @@ export default {
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.57);
 }
 
+.cart {
+  margin: 25px 100px;
+  float: right;
+  border: 1px solid #d8d8d8;
+  padding: 30px 30px;
+  background-color: white;
+  -webkit-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  -moz-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  box-shadow: 2px 15px -12px rgba(0, 0, 0, 0.57);
+}
+
+.button {
+  margin: 30px;
+  background-color: #39495c;
+  border-radius: 5px;
+  font-size: 18px;
+  width: 160px;
+  height: 60px;
+  color: white;
+  padding: 20px;
+  box-shadow: inset 0 -0.6em 1em -0.35em rgba(0, 0, 0, 0.17),
+    inset 0 0.6em 2em -0.3em rgba(255, 255, 255, 0.15),
+    inset 0 0 0em 0.05em rgba(255, 255, 255, 0.12);
+  text-align: center;
+  cursor: pointer;
+}
+.color-circle {
+  width: 50px;
+  height: 50px;
+  margin-top: 8px;
+  border: 2px solid #d8d8d8;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+.disabledButton {
+  background-color: #d8d8d8;
+  cursor: not-allowed;
+}
 .product-display {
   display: flex;
   flex-direction: column;
@@ -100,6 +156,11 @@ h3 {
 
 p {
   font-size: 22px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
 }
 
 .product-image,
